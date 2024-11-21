a11fe03fe5cfa9bab657bed2e8d2a33465ae118a
aoninoa <aoninoa@proton.me>
2024-11-21 10:13:15 +0900

    Implement cookie

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import { findOne } from "./data/users.js";
 import config from "./config/config.js";
@@ -22,25 +23,35 @@ const generateToken = (user) => {
 };
 
 const authenticateToken = (req, res, next) => {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (authHeader === undefined) {
+  if (req.cookies.token) {
+    const token = req.cookies.token;
+    jwt.verify(token, jwtTokenSecret, (err, user) => {
+      if (err) {
+        return res.status(400).json({ success: false, error: err.message });
+      }
+      req.user = user;
+      return next();
+    });
+  } else {
     return res.status(401).json({ success: false, error: "Authorization header is not set" });
-  }
-  /*
-   * Authorization header looks like: Bearer <token>
-   * so split header with white space and take the second one
-   */
-  const token = authHeader.split(" ")[1];
+    // const authHeader = req.headers.authorization;
+    // if (authHeader === undefined) {
+    //   return res.status(401).json({ success: false, error: "Authorization header is not set" });
+    // }
+    // /*
+    //  * Authorization header looks like: Bearer <token>
+    //  * so split header with white space and take the second one
+    //  */
+    // const token = authHeader.split(" ")[1];
 
-  jwt.verify(token, jwtTokenSecret, (err, user) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({ error: err.message });
-    }
-    req.user = user;
-    next();
-  });
+    // jwt.verify(token, jwtTokenSecret, (err, user) => {
+    //   if (err) {
+    //     return res.status(400).json({ error: err.message });
+    //   }
+    //   req.user = user;
+    //   return next();
+    // });
+  }
 };
 
 const app = express();
@@ -48,12 +59,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 app.get("/", (_req, res) => {
   res.sendFile(abspath("../public/index.html"));
 });
 
-app.get("/secret", authenticateToken, (req, res) => {
+app.get("/protected", authenticateToken, (req, res) => {
   if (req.user) {
     res.json(req.user);
   } else {
@@ -63,6 +75,9 @@ app.get("/secret", authenticateToken, (req, res) => {
 
 app.post("/login", (req, res) => {
   const { userID, password } = req.body;
+  if (!(userID && password) ) {
+    return res.status(400).json({ success: false, error: "User ID or Password or both are invalid" });
+  }
   const user = findOne(userID);
   if (!user) {
     return res.status(401).json({ success: false, error: `${userID} is not found` });
@@ -72,7 +87,12 @@ app.post("/login", (req, res) => {
   }
   user.password = undefined;
   const token = generateToken(user);
-  res.json({ success: true, token });
+  res.
+    cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  })
+  .json({ success: true, token });
 });
 
 app.listen(port, () => {
